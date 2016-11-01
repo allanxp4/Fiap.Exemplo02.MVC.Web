7708dfb2c0dd48820752e9ccf03eda0ac0d1c58d
Allan <allanxp4@gmail.com>
2016-11-01 18:23:51 -0200

    Initial Commit

---
+++ Fiap.Exemplo02.Database/Aluno.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Aluno]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Nome] NVARCHAR(200) NOT NULL, 
+    [DataNascimento] NCHAR(10) NOT NULL, 
+    [Bolsista] BIT NOT NULL, 
+    [Desconto] FLOAT NULL
+)
